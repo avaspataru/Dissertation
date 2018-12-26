97111da9724ed6eb89663416abb2a10ba49a6ac6
avaspataru <ava.spataru@yahoo.com>
2018-12-26 12:56:09 +0200

    finished analysis for new dataset

--- new dataset/first.R
+++ new dataset/first.R
@@ -19,10 +19,10 @@ row.names(new.data) <- genes
 # genes expressed in >= 3 cells (~0.1% of the data). Keep all cells with at
 # least 200 detected genes
 #eliminate those expressed in < 1 cells
-dataobj <- CreateSeuratObject(raw.data = new.data, min.cells = 1, min.genes = 200,project = "NEWDATA2")
+dataobj <- CreateSeuratObject(raw.data = new.data, min.cells = 3, min.genes = 200,project = "NEWDATA2")
 
 #STEP 2
-mito.genes <- grep(pattern = "^MT-", x = genes, value = TRUE)
+mito.genes <- grep(pattern = "^MT-", x = rownames(x = dataobj@data), value = TRUE)
 percent.mito <- Matrix::colSums(dataobj@raw.data[mito.genes, ])/Matrix::colSums(dataobj@raw.data)
 
 #CellsMeta = data@data.info
@@ -30,8 +30,8 @@ percent.mito <- Matrix::colSums(dataobj@raw.data[mito.genes, ])/Matrix::colSums(
 dataobj <- AddMetaData(object = dataobj, metadata = percent.mito, col.name = "percent.mito")
 par(mfrow = c(1, 2))
 
-#dataobj <- FilterCells(object = dataobj, subset.names = c("percent.mito"), 
-#                    low.thresholds = c(0.0), high.thresholds = c(0.05))
+dataobj <- FilterCells(object = dataobj, subset.names = c("percent.mito"), 
+                    low.thresholds = c(0.0), high.thresholds = c(0.5))
 
 dataobj <- NormalizeData(object = dataobj, normalization.method = "LogNormalize", 
                       scale.factor = 10000)
@@ -42,7 +42,7 @@ dataobj <- NormalizeData(object = dataobj, normalization.method = "LogNormalize"
 #GenePlot(object = dataobj, gene1 = "nUMI", gene2 = "nGene")
 
 dataobj <- FindVariableGenes(object = dataobj, mean.function = ExpMean, dispersion.function = LogVMR, 
-                          x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
+                          x.low.cutoff = 0.3, x.high.cutoff = 3, y.cutoff = 0.5)
 #length(x = dataobj@var.genes)
 
 dataobj <- ScaleData(object = dataobj)
@@ -51,8 +51,20 @@ dataobj <- RunPCA(object = dataobj, pc.genes = dataobj@var.genes,
                   do.print = TRUE, pcs.print = 1:100, genes.print = 5)
 
 dataobj <- FindClusters(object = dataobj, reduction.type = "pca", dims.use = 1:10, 
-                     resolution = 0.6, print.output = 0, save.SNN = TRUE)
+                     resolution = 0.6, print.output = 0, save.SNN = TRUE, n.iter=5000, k.param=10)
 
 PrintFindClustersParams(object = dataobj)
-dataobj <- RunTSNE(object = dataobj, dims.use = 1:10, do.fast = TRUE,  check_duplicates = FALSE)
+dataobj <- RunTSNE(object = dataobj, dims.use = 1:10, do.fast = TRUE,  check_duplicates = FALSE, n.iter=5000, perplexity=10)
 TSNEPlot(object = dataobj)
+
+
+# find markers for every cluster compared to all remaining cells, report
+# only the positive ones
+dataobj.markers <- FindAllMarkers(object = dataobj, only.pos = TRUE, min.pct = 0.25, 
+                               thresh.use = 0.25)
+dataobj.markers %>% group_by(cluster) %>% top_n(2, avg_logFC)
+
+top10 <- dataobj.markers %>% group_by(cluster) %>% top_n(10, avg_logFC)
+# setting slim.col.label to TRUE will print just the cluster IDS instead of
+# every cell name
+DoHeatmap(object = dataobj, genes.use = top10$gene, slim.col.label = TRUE, remove.key = TRUE)
